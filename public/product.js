9f8a59537d33d5cff41c36922c97e25a57390152
Sowbhakian <sowbhakian.shansan@gmail.com>
2022-08-15 11:08:51 +0530

    Final File

--- public/product.js
+++ public/product.js
@@ -2,87 +2,105 @@ export function productsList() {
     const products = [{
 
         "DAIRY": [{
-                ProductName: "Panner",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Paneer",
+                Des: "Paneer is a fresh cheese used in a wide variety of Indian recipes, made by heating and then curdling milk using acid. It's very mild and milky in flavor, white in color, and its texture is soft, spongy, and squeaky. This texture helps it to absorb the flavors of sauces or marinades.",
+                Price: "70"
             },
             {
-                ProductName: "Flovoured Mik",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Flovoured Milk",
+                Des: "Flavored milk is a sweetened dairy drink made with milk, sugar, flavorings, and sometimes food colorings.",
+                Price: "90"
             },
             {
                 ProductName: "Butter",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "butter-It is a dairy product made by churning either fresh fermented cream or milk.Butter contributes desirable flavor and texture to food and bakery products.",
+                Price: "50"
             },
             {
-                ProductName: "Mexican Salsa Sauce ",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Curd ",
+                Des: "Refreshing, creamy curd made from pure milk and zero chemicals ID natural curd is made without the use of artificial culture Simply scoop from the pack and use",
+                Price: "60"
             },
             {
-                ProductName: "Cova",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Khova",
+                Des: "khov- It is made of either dried whole milk or milk thickened by heating in an open iron pan. It is lower in moisture than typical fresh cheeses .",
+                Price: "40"
             },
 
         ]
     }, {
         "FILLING": [{
                 ProductName: "Caramel Topping",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Caramel is a confectionery product used for flavoring, topping and filling in variety of foods and beverages.",
+                Price: "290"
             },
             {
                 ProductName: "Thick Chocolate Syrup",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Chocolate syrup is a sweet, chocolate-flavored condiment. It is often used as a topping or dessert sauce for various desserts, such as ice cream, or mixed with milk to make chocolate milk or blended with milk and ice cream to make a chocolate milkshake.",
+                Price: "280"
             },
             {
                 ProductName: "Pancake Syrup",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Maple syrup is a sweet, sticky, brown liquid made from the sap of maple trees, that can be eaten with pancakes or used to make desserts.",
+                Price: "300"
             }
 
         ]
     }, {
         "FROZENNV": [{
                 ProductName: "Chicken Burger Patty",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "A hot sandwich made of a patty of chicken in a bun, often with other ingredients.",
+                Price: "140"
             },
             {
                 ProductName: "Chicken Salami Roll",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Tasty and healthy Chicken Salami is well Textured, Aromatic Chicken Salami with an Excellent Flavor Profile. This fully cooked salami is a great way to come up with a fantastic, hassle-free meal anytime!",
+                Price: "200"
             },
             {
-                ProductName: "chicken sheek kabab",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Chicken sheek kabab",
+                Des: "Chicken Seekh Kabab is the special delight of minced chicken, poppy seeds and spices. The nice mixture of minced chicken is simply molded onto the skewers and cooked in the hot oil. Learn the easiest way to make tastiest chicken seekh kabab in your own kitchen.",
+                Price: "150"
             },
             {
                 ProductName: "Chicken Sausage",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Chicken sausage is a type of sausage made from ground chicken meat. It is usually made from dark meat, and is often flavored with sage, parsley, or other herbs. It's high in protein and low in fat, and is a good source of iron and zinc.",
+                Price: "190"
 
             },
             {
                 ProductName: "meat balls",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "It's an exceptionally accessible dish, simple and affordable. Meatballs can be made with nearly any kind of meat, and since that meat is ground and mixed with herbs and other flavors, cheap cuts of meat can be transformed into something delicious.",
+                Price: "160"
 
             }
         ]
     }, {
         "FROZENV": [{
                 ProductName: "Veg Spring Roll",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Eating spring rolls is a way to welcome the arrival of spring. The golden cylindrical-shaped rolls represent gold bars — which symbolize wealth.",
+                Price: "89"
             },
             {
                 ProductName: "Veg momos",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "These round soft dough balls are filled with juicy veg stuffings and are served with a spicy dip.",
+                Price: "124"
             },
             {
                 ProductName: "Veg Nuggets",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Veggie Nuggets is an amazing combination of potatoes, vegetables and a bold tandoori seasoning, that is finely coated in crispy golden bread crumbs, our Veggie Nuggets will make sure that you enjoy every bite thoroughly!",
+                Price: "78"
             },
             {
-                ProductName: "Panner Tatters",
-                Des: "asd asd asd asd asd asd asd"
+                ProductName: "Paneer Tatters",
+                Des: "Paneer TATTERS-- is an Indian dish made from chunks of paneer marinated in spices and grilled in a tandoor.",
+                Price: "132"
 
             },
             {
                 ProductName: "French Fries",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "FRENCH FIRESnack typically made from deep-fried potatoes that have been cut into various shapes, especially thin strips. Fries are often salted and served",
+                Price: "141"
 
             }
         ]
@@ -90,27 +108,46 @@ export function productsList() {
     }, {
         "TINNED": [{
                 ProductName: "Mango Pulp",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Mango pulp is the inner fleshy yellow, sweet part of the mango. Pulp of mango can be easily extracted by peeling any variety of mango and then crushing it. In order to extract mango pulp, crush using your hands or put the pieces in a mixer or food processor and blend to a pulp.",
+                Price: "250"
             },
             {
                 ProductName: "Mushroom ",
-                Des: "asd asd asd asd asd asd asd"
-            },
-            {
-                ProductName: "Veg Nuggets",
-                Des: "asd asd asd asd asd asd asd"
-            },
-            {
-                ProductName: "Lychee",
-                Des: "asd asd asd asd asd asd asd"
-
+                Des: "Mushrooms are a rich, low calorie source of fiber, protein, and antioxidants.",
+                Price: "237"
             },
             {
                 ProductName: "Gherkins",
-                Des: "asd asd asd asd asd asd asd"
+                Des: "Gherkins are a specific kind of cucumber. They are the kind that is most often used for pickling.",
+                Price: "126"
+            },
+            {
+                ProductName: "Lychee",
+                Des: "The aromatic lychee is sweet, with slightly floral and acidic notes. Upon biting, you'll get a juicy flavor burst with a taste similar to a strawberry or pear with a hint of citrus. Some also compare the floral taste to rose.",
+                Price: "180"
+
+            },
+            {
+                ProductName: "Red Cherry",
+                Des: "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe. Commercial cherries are obtained from cultivars of several species, such as the sweet Prunus avium and the sour Prunus cerasus.",
+                Price: "325"
 
             }
         ]
+    }, {
+        "CULLINARY": [{
+            ProductName: "Sweet Chilli Sauce",
+            Des: "It is commonly made with red chili peppers , rice wine vinegar, sometimes garlic, sometimes fish sauce, and a sweetening ingredient such as fruit or a refined sugar or honey.",
+            Price: "190"
+        }, {
+            ProductName: "Mexican Salsa Sauce",
+            Des: "A salsa is a mixture of chopped vegetables or fruit with a variety of flavourings. A side dish originating in Mexican cuisine, it is now popular served with barbequed meats, chicken or seafood. Tomatoes are often used as the base and combined with cucumber, onion, garlic, herbs and lemon or lime juice.",
+            Price: "210"
+        }, {
+            ProductName: "Tomato Sauce",
+            Des: "A tomato sauce consists of chopped tomatoes  in olive oil and simmered until they lose their raw flavor, seasoned to taste with salt, or other herbs or spices.",
+            Price: "200"
+        }]
     }]
 
     return products
